7864416018cbcc79b58100e87d9578d6379441fd
Naruebet <naruebet@gmail.com>
2024-06-22 05:43:13 +0700

    Python Tools

---
+++ Tools/ExportLOC.bat
@@ -0,0 +1,5 @@
+@echo off
+D:\python\python.exe D:\AppPool\CS2THMod\Tools\ExpEN.py
+D:\python\python.exe D:\AppPool\CS2THMod\Tools\ExpKR.py
+D:\python\python.exe D:\AppPool\CS2THMod\Tools\ExpHANS.py
+php txt2csv.php
--- Tools/GenTHLoc.bat
+++ Tools/GenTHLoc.bat
@@ -1,6 +1,7 @@
 @echo off
 php sheet2txt.php
 copy D:\AppPool\CS2THMod\Sources\Locale\th-TH.loc.txt "D:\AppPool\CS2THMod\Tools\th-TH.loc.txt"
+copy D:\AppPool\CS2THMod\Sources\Locale\en-US.loc "D:\AppPool\CS2THMod\Tools\en-US.loc"
 D:\python\python.exe D:\AppPool\CS2THMod\Tools\GenLOC.py
 copy D:\AppPool\CS2THMod\Tools\th-TH.loc "D:\AppPool\CS2THMod\Sources\Locale\th-TH.loc"
 copy D:\AppPool\CS2THMod\Tools\th-TH.loc "E:\SteamLibrary\steamapps\common\Cities Skylines II\Cities2_Data\StreamingAssets\Data~\th-TH.loc"
